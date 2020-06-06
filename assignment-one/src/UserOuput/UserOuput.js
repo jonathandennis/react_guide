ed8fc633715f7b013d339827dd44f0ea12bfa0ce
jonathandennis <4jondennis@gmail.com>
2020-06-06 18:49:10 +0200

    react_guide

---
+++ assignment-one/src/UserOuput/UserOuput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Some text</p>
+            <p>Some more</p>
+        </div>
+    );
+};
+
+export default userOutput;
